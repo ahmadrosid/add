98c86e029924f3d2a4a60a9cf66acee86b9359dd
ahmadrosid <alahmadrosid@gmail.com>
2020-03-08 01:15:10 +0700

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">Add</h1>
+<p align="center"><b>Add</b> is a tiny commandline application to make file or folder easily.</p>
+
+## ⚡️ Quickstart
+```bash
+add makan/nasi.lua
+Result :
+├── makan
+│   └── nasi.lua
+```
+
+## Installation ⚙️ 
+First of all you need to have `luarocks` installed see [Development](#development-).
+
+Installation is done by using `luarocks install` :
+```bash
+luarocks install add
+```
+
+## License 📄 
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT) - see the [LICENSE](LICENSE) file for details.
---
+++ add.lua
@@ -0,0 +1,34 @@
+local input = arg[1]
+local paths = {}
+
+local function is_dir(name)
+	return os.rename(name,name) and true or false
+end
+
+if not input then
+	print("please type filename")
+end
+
+for path in string.gmatch(input, "[^/]+") do
+	paths[#paths + 1] = path
+end
+
+if #paths == 1 then
+	if string.match(input, "/") then
+		os.execute("mkdir " .. input)
+	else
+		io.open(input, "w")
+	end
+end
+
+local dir_paths = ""
+for index, value in ipairs(paths) do
+	if index == #paths then
+		io.open(dir_paths .. value, "w")
+	else
+		dir_paths = dir_paths .. value .. "/"
+		if not is_dir(dir_paths) then
+			os.execute("mkdir " .. dir_paths)
+		end
+	end
+end
